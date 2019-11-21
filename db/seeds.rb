f04b520d2d885a0742d88e9c17e642656eabc2ef
chiara tinmazian <chiara.tinmazian@gmail.com>
2019-11-21 18:52:17 +0100

    oui

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "destroying all ingredients"
+Ingredient.destroy_all
+
+puts "Creating ingredients"
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
